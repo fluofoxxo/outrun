83a844455d61e60edb0dd5c787d4c6b9cf0a618e
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-08-09 13:27:24 -0400

    Add handler for unknown requests

--- mess1.go
+++ mess1.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/fluofoxxo/outrun/cryption"
 	"github.com/fluofoxxo/outrun/handlers"
 )
+
+const UNKNOWN_REQUEST_DIRECTORY = "logging/unknown_requests/"
 
 /*
 !!!!!!!!!!
@@ -16,6 +23,23 @@ WHEN THE USER CHOOSES A USERNAME, MAKE SURE TO SET usernameLookup/username = UID
 AND MAKE SURE THAT THE USERNAME DOESN'T EXIST
 !!!!!!!!!!
 */
+
+func OutputUnknownRequest(w http.ResponseWriter, r *http.Request) {
+	recv := cryption.GetReceivedMessage(r)
+	// make a new logging path
+	timeStr := strconv.Itoa(int(time.Now().Unix()))
+	os.MkdirAll(UNKNOWN_REQUEST_DIRECTORY, 0644)
+	normalizedReq := strings.ReplaceAll(r.URL.Path, "/", "-")
+	path := UNKNOWN_REQUEST_DIRECTORY + normalizedReq + "_" + timeStr + ".txt"
+	err := ioutil.WriteFile(path, recv, 0644)
+	if err != nil {
+		log.Println("[OUT] UNABLE TO WRITE UNKNOWN REQUEST: " + err.Error())
+		w.Write([]byte(""))
+		return
+	}
+	log.Println("[OUT] !!!!!!!!!!!! Unknown request, output to " + path)
+	w.Write([]byte(""))
+}
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -45,5 +69,7 @@ func main() {
 	mux.HandleFunc("/Player/setUserName/", handlers.SetUserNameHandler)
 	mux.HandleFunc("/login/getTicker/", handlers.GetTickerHandler)
 	mux.HandleFunc("/Login/loginBonus/", handlers.LoginBonusHandler)
+
+	mux.HandleFunc("/", OutputUnknownRequest)
 	panic(http.ListenAndServe(":9001", mux))
 }
