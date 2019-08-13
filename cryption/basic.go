13cd558be16999effd7e846bcf3f74da3b17bad2
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-08-13 15:07:55 -0400

    Code layout definition 1

---
+++ cryption/basic.go
@@ -0,0 +1,33 @@
+package cryption
+
+import (
+    "log"
+    "net/http"
+    "regexp"
+)
+
+var EncryptionIv = []byte("")
+var EncryptionKey = []byte("Ec7bLaTdSuXuf5pW")
+
+func CleanBytes(b []byte) []byte {
+    re := regexp.MustCompile("[^\u0020-\u007f]+")
+    return []byte(re.ReplaceAllLiteralString(string(b), ""))
+}
+
+func GetReceivedMessage(r *http.Request) []byte {
+    err := r.ParseForm()
+    if err != nil {
+        log.Println("[ERR] Error in parsing form: " + err.Error())
+    }
+    param := r.Form.Get("param")
+    iv := r.Form.Get("key")
+    secure := r.Form.Get("secure")
+    if secure != "1" {
+        return []byte(param) // message is not encrypted!
+    }
+    EncryptionIv = []byte(iv)
+    paramUnB64 := B64Decode(param)
+    decrypted := Decrypt(paramUnB64, EncryptionKey, EncryptionIv)
+    decrypted = CleanBytes(decrypted)
+    return decrypted
+}
