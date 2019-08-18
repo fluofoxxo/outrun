2478bb0e3c975053f4d228a38e6024104b015dbb
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-08-18 00:52:24 -0400

    Shorten output of function name by helper

--- muxhandlers/muxobj/objMuxHandler.go
+++ muxhandlers/muxobj/objMuxHandler.go
@@ -4,13 +4,15 @@ import (
 	"net/http"
 	"reflect"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/fluofoxxo/outrun/helper"
 )
 
 func Handle(f func(*helper.Helper), logExecutionTime bool) func(w http.ResponseWriter, r *http.Request) {
-	funcname := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+	funcnameSplit := strings.Split(runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name(), "/")
+	funcname := funcnameSplit[len(funcnameSplit)-1]
 	if logExecutionTime {
 		return func(w http.ResponseWriter, r *http.Request) {
 			startTime := time.Now()
