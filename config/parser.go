338a547199fe38475e506b468876933a88c0d25b
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-09-06 16:50:14 -0400

    Add more config options, add debug code

--- config/parser.go
+++ config/parser.go
@@ -1,58 +1,68 @@
 package config
 
 import (
-    "encoding/json"
-    "io/ioutil"
+	"encoding/json"
+	"io/ioutil"
 )
 
 // defaults
 const (
-    DPort               = "9001"
-    DDoTimeLogging      = true
-    DLogUnknownRequests = true
+	DPort               = "9001"
+	DDoTimeLogging      = true
+	DLogUnknownRequests = true
+	DLogAllRequests     = false
+	DDebug              = false
 )
 
 var CFile ConfigFile
 
 type ConfigFile struct {
-    Port               string `json:"port"`
-    DoTimeLogging      bool   `json:"doTimeLogging"`
-    LogUnknownRequests bool   `json:"logUnknownRequests"`
+	Port               string `json:"port"`
+	DoTimeLogging      bool   `json:"doTimeLogging"`
+	LogUnknownRequests bool   `json:"logUnknownRequests"`
+	LogAllRequests     bool   `json:"logAllRequests"`
+	Debug              bool   `json:"debug"`
 }
 
 func Parse(filename string) error {
-    port := DPort
-    doTimeLogging := DDoTimeLogging
-    logUnknownRequests := DLogUnknownRequests
+	port := DPort
+	doTimeLogging := DDoTimeLogging
+	logUnknownRequests := DLogUnknownRequests
+	logAllRequests := DLogAllRequests
+	debug := DDebug
 
-    var ret error
-    file, err := loadFile(filename)
-    if err == nil {
-        var cf ConfigFile
-        err := json.Unmarshal(file, &cf)
-        if err == nil {
-            port = cf.Port
-            doTimeLogging = cf.DoTimeLogging
-            logUnknownRequests = cf.LogUnknownRequests
-        } else {
-            ret = err
-        }
-    } else {
-        ret = err
-    }
+	var ret error
+	file, err := loadFile(filename)
+	if err == nil {
+		var cf ConfigFile
+		err := json.Unmarshal(file, &cf)
+		if err == nil {
+			port = cf.Port
+			doTimeLogging = cf.DoTimeLogging
+			logUnknownRequests = cf.LogUnknownRequests
+			logAllRequests = cf.LogAllRequests
+			debug = cf.Debug
+		} else {
+			ret = err
+		}
+	} else {
+		ret = err
+	}
 
-    CFile = ConfigFile{
-        port,
-        doTimeLogging,
-        logUnknownRequests,
-    }
-    return ret
+	CFile = ConfigFile{
+		port,
+		doTimeLogging,
+		logUnknownRequests,
+		logAllRequests,
+		debug,
+	}
+	return ret
 }
 
 func loadFile(filename string) ([]byte, error) {
-    b, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return []byte{}, err
-    }
-    return b, err
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return []byte{}, err
+	}
+	return b, err
 }
--- muxhandlers/muxobj/objMuxHandler.go
+++ muxhandlers/muxobj/objMuxHandler.go
@@ -1,12 +1,15 @@
 package muxobj
 
 import (
+	"io/ioutil"
 	"net/http"
 	"reflect"
 	"runtime"
+	"strconv"
 	"strings"
 	"time"
 
+	"github.com/fluofoxxo/outrun/config"
 	"github.com/fluofoxxo/outrun/helper"
 )
 
@@ -19,6 +22,18 @@ func Handle(f func(*helper.Helper), logExecutionTime bool) func(w http.ResponseW
 		return func(w http.ResponseWriter, r *http.Request) {
 			startTime := time.Now()
 			help := helper.MakeHelper(funcname, w, r)
+			if config.CFile.Debug {
+				nano := time.Now().UnixNano()
+				nanoStr := strconv.Itoa(int(nano))
+				filename := help.Request.RequestURI + "--" + nanoStr + ".txt"
+				filename = strings.ReplaceAll(filename, ".", "-")
+				filename = strings.ReplaceAll(filename, "/", "-")
+				help.Out("DEBUG: Saving request to " + filename)
+				err := ioutil.WriteFile(filename, help.GetGameRequest(), 0644)
+				if err != nil {
+					help.Out("DEBUG ERROR: Unable to write file '" + filename + "'")
+				}
+			}
 			f(help)
 			endTime := time.Now()
 			timeDiff := endTime.Sub(startTime)
