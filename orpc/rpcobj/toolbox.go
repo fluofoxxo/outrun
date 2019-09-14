8d932e38e2dfd6a674e681ba1fb1f282b23dcf9e
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-09-13 23:11:11 -0400

    RPC campaign functions

--- orpc/rpcobj/toolbox.go
+++ orpc/rpcobj/toolbox.go
@@ -1,6 +1,11 @@
 package rpcobj
 
-import "github.com/fluofoxxo/outrun/db"
+import (
+	"strconv"
+
+	"github.com/fluofoxxo/outrun/db"
+	"github.com/fluofoxxo/outrun/netobj"
+)
 
 type Toolbox struct {
 }
@@ -15,6 +20,37 @@ func (t *Toolbox) RegisterPlayerWithID(uid string, reply *ToolboxReply) error {
 	}
 	reply.Status = StatusOK
 	reply.Info = "OK"
+	return nil
+}
+
+func (t *Toolbox) ResetCampaign(uid string, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	player.MileageMapState = netobj.DefaultMileageMapState()
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to save player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
+}
+
+func (t *Toolbox) Debug_GetCampaignStatus(uid string, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = strconv.Itoa(int(player.MileageMapState.Chapter)) + "," + strconv.Itoa(int(player.MileageMapState.Episode)) + "," + strconv.Itoa(int(player.MileageMapState.Point))
 	return nil
 }
 
