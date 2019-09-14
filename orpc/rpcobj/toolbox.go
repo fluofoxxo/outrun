27eecbb099115b35c5d016b13b6b4d120e8f08ac
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-09-14 16:16:18 -0400

    RPC: Add SetRings, SetRedRings

--- orpc/rpcobj/toolbox.go
+++ orpc/rpcobj/toolbox.go
@@ -45,6 +45,82 @@ func (t *Toolbox) ResetCampaign(uid string, reply *ToolboxReply) error {
 	return nil
 }
 
+func (t *Toolbox) SetRings(args ChangeValueArgs, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(args.UID)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	newRings := args.Value.(int64)
+	player.PlayerState.NumRings = newRings
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to save player: " + err.Error()
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
+}
+
+func (t *Toolbox) SetRedRings(args ChangeValueArgs, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(args.UID)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	newRedRings := args.Value.(int64)
+	player.PlayerState.NumRedRings = newRedRings
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to save player: " + err.Error()
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
+}
+
+func (t *Toolbox) SetMainCharacter(args ChangeValueArgs, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(args.UID)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	newMainCharaID := args.Value.(string)
+	player.PlayerState.MainCharaID = newMainCharaID
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to save player: " + err.Error()
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
+}
+
+func (t *Toolbox) SetSubCharacter(args ChangeValueArgs, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(args.UID)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	newSubCharaID := args.Value.(string)
+	player.PlayerState.SubCharaID = newSubCharaID
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to save player: " + err.Error()
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
+}
+
 func (t *Toolbox) Debug_GetCampaignStatus(uid string, reply *ToolboxReply) error {
 	player, err := db.GetPlayer(uid)
 	if err != nil {
@@ -72,4 +148,9 @@ func (t *Toolbox) Debug_GetAllPlayerIDs(nothing bool, reply *ToolboxReply) error
 type ToolboxReply struct {
 	Status uint
 	Info   string
+}
+
+type ChangeValueArgs struct {
+	UID   string
+	Value interface{}
 }
