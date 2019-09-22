1f6570d63a6b7b62505fdb4186893aef18ec0c4b
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-09-21 21:28:28 -0400

    RPC: Add ResetMapInfo

--- orpc/rpcobj/toolbox.go
+++ orpc/rpcobj/toolbox.go
@@ -8,6 +8,7 @@ import (
 	"github.com/fluofoxxo/outrun/db"
 	"github.com/fluofoxxo/outrun/db/dbaccess"
 	"github.com/fluofoxxo/outrun/netobj"
+	"github.com/fluofoxxo/outrun/obj"
 )
 
 type Toolbox struct {
@@ -226,6 +227,25 @@ func (t *Toolbox) ResetPlayerVarious(uid string, reply *ToolboxReply) error {
 		return err
 	}
 	player.PlayerVarious = netobj.DefaultPlayerVarious()
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to save player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
+}
+
+func (t *Toolbox) ResetMapInfo(uid string, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	player.MileageMapState.MapInfo = obj.DefaultMapInfo()
 	err = db.SavePlayer(player)
 	if err != nil {
 		reply.Status = StatusOtherError
