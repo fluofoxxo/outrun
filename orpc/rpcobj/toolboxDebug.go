868164f00776204150f4701c9bc6384764baa947
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-10-16 12:47:42 -0400

    RPC: Add Debug_MigratePlayer

--- orpc/rpcobj/toolboxDebug.go
+++ orpc/rpcobj/toolboxDebug.go
@@ -1,126 +1,166 @@
 package rpcobj
 
 import (
-    "encoding/json"
-    "strconv"
-    "strings"
+	"encoding/json"
+	"strconv"
+	"strings"
 
-    "github.com/fluofoxxo/outrun/consts"
-    "github.com/fluofoxxo/outrun/db"
-    "github.com/fluofoxxo/outrun/db/dbaccess"
-    "github.com/fluofoxxo/outrun/netobj"
-    "github.com/fluofoxxo/outrun/netobj/constnetobjs"
+	"github.com/fluofoxxo/outrun/consts"
+	"github.com/fluofoxxo/outrun/db"
+	"github.com/fluofoxxo/outrun/db/dbaccess"
+	"github.com/fluofoxxo/outrun/netobj"
+	"github.com/fluofoxxo/outrun/netobj/constnetobjs"
 )
 
 func (t *Toolbox) Debug_GetCampaignStatus(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = strconv.Itoa(int(player.MileageMapState.Chapter)) + "," + strconv.Itoa(int(player.MileageMapState.Episode)) + "," + strconv.Itoa(int(player.MileageMapState.Point))
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = strconv.Itoa(int(player.MileageMapState.Chapter)) + "," + strconv.Itoa(int(player.MileageMapState.Episode)) + "," + strconv.Itoa(int(player.MileageMapState.Point))
+	return nil
 }
 
 func (t *Toolbox) Debug_GetAllPlayerIDs(nothing bool, reply *ToolboxReply) error {
-    playerIDs := []string{}
-    dbaccess.ForEachKey(consts.DBBucketPlayers, func(k, v []byte) error {
-        playerIDs = append(playerIDs, string(k))
-        return nil
-    })
-    final := strings.Join(playerIDs, ",")
-    reply.Status = StatusOK
-    reply.Info = final
-    return nil
+	playerIDs := []string{}
+	dbaccess.ForEachKey(consts.DBBucketPlayers, func(k, v []byte) error {
+		playerIDs = append(playerIDs, string(k))
+		return nil
+	})
+	final := strings.Join(playerIDs, ",")
+	reply.Status = StatusOK
+	reply.Info = final
+	return nil
 }
 
 func (t *Toolbox) Debug_ResetPlayer(uid string, reply *ToolboxReply) error {
-    _ = db.NewAccountWithID(uid)
-    reply.Status = StatusOK
-    reply.Info = "OK"
-    return nil
+	_ = db.NewAccountWithID(uid)
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
 }
 
 func (t *Toolbox) Debug_GetRouletteInfo(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    rouletteInfo := player.RouletteInfo
-    jri, err := json.Marshal(rouletteInfo)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to marshal RouletteInfo: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = string(jri)
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	rouletteInfo := player.RouletteInfo
+	jri, err := json.Marshal(rouletteInfo)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to marshal RouletteInfo: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = string(jri)
+	return nil
 }
 
 func (t *Toolbox) Debug_ResetChaoRouletteGroup(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    chaoRouletteGroup := netobj.DefaultChaoRouletteGroup(player.PlayerState, player.GetAllNonMaxedCharacters(), player.GetAllNonMaxedChao())
-    player.ChaoRouletteGroup = chaoRouletteGroup
-    err = db.SavePlayer(player)
-    if err != nil {
-        reply.Status = StatusOK
-        reply.Info = "OK"
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = "OK"
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	chaoRouletteGroup := netobj.DefaultChaoRouletteGroup(player.PlayerState, player.GetAllNonMaxedCharacters(), player.GetAllNonMaxedChao())
+	player.ChaoRouletteGroup = chaoRouletteGroup
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOK
+		reply.Info = "OK"
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
 }
 
 func (t *Toolbox) Debug_ResetCharactersAndCompensate(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    toAdd := int64(0)
-    for _, char := range player.CharacterState {
-        toAdd += char.Level * 15
-    }
-    player.PlayerState.NumRedRings += toAdd
-    player.CharacterState = netobj.DefaultCharacterState()
-    err = db.SavePlayer(player)
-    if err != nil {
-        reply.Status = StatusOK
-        reply.Info = "OK"
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = "OK"
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	toAdd := int64(0)
+	for _, char := range player.CharacterState {
+		toAdd += char.Level * 15
+	}
+	player.PlayerState.NumRedRings += toAdd
+	player.CharacterState = netobj.DefaultCharacterState()
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOK
+		reply.Info = "OK"
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
 }
 
 func (t *Toolbox) Debug_ResetChao(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    player.ChaoState = constnetobjs.NetChaoList
-    err = db.SavePlayer(player)
-    if err != nil {
-        reply.Status = StatusOK
-        reply.Info = "OK"
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = "OK"
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	player.ChaoState = constnetobjs.NetChaoList
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOK
+		reply.Info = "OK"
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
+}
+
+func (t *Toolbox) Debug_MigrateUser(uidToUID string, reply *ToolboxReply) error {
+	uidSrc := strings.Split(uidToUID, "->")
+	if len(uidSrc) != 2 {
+		reply.Status = StatusOtherError
+		reply.Info = "improperly formatted string (Example: 1234567890->1987654321)"
+	}
+
+	fromUID := uidSrc[0]
+	toUID := uidSrc[1]
+	playerSrc, err := dbaccess.Get(consts.DBBucketPlayers, fromUID)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = err.Error()
+		return err
+	}
+	err = dbaccess.Set(consts.DBBucketPlayers, toUID, playerSrc)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = err.Error()
+		return err
+	}
+	newPlayer, err := db.GetPlayer(toUID)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = err.Error()
+		return err
+	}
+	newPlayer.ID = toUID
+	err = db.SavePlayer(newPlayer)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = err.Error()
+		return err
+	}
+
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
 }
