71467e69ca3a8eb93f7c07d145a9ebf4cc928454
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-10-20 17:24:01 -0400

    Remove duplicate Chao check

--- logic/roulette/randomChaoWheelChoice.go
+++ logic/roulette/randomChaoWheelChoice.go
@@ -2,12 +2,9 @@ package roulette
 
 import (
 	"errors"
-	"log"
 	"math/rand"
 	"strconv"
-	"time"
 
-	"github.com/fluofoxxo/outrun/config"
 	"github.com/fluofoxxo/outrun/consts"
 	"github.com/fluofoxxo/outrun/enums"
 )
@@ -85,27 +82,31 @@ func GetRandomChaoWheelChao(rarity int64, count int) ([]string, error) {
 }
 
 func GetRandomChaoRouletteItems(rarities []int64, allowedCharacters, allowedChao []string) ([]string, []int64, error) { // TODO: Possibly rename to GetRandomChaoWheelItems?
-	failsafeTimeout := int64(3) // seconds, used to break in case of infinite loop
-	debugOriginalTime := time.Now().Unix()
+	//failsafeTimeout := int64(3) // seconds, used to break in case of infinite loop
+	//debugOriginalTime := time.Now().Unix()
 
-	dprint := func(a ...interface{}) {
+	/*dprint := func(a ...interface{}) {
 		if config.CFile.DebugPrints {
 			log.Println(a...)
 		}
-	}
+	}*/
 	allowedRarity := func(x int) bool {
-		if x == 100 { // character
-			return len(allowedCharacters) != 0 // if any characters allowed, return true
-		}
-		searchingForRarity := strconv.Itoa(x)[0] // get byte
-		for _, chao := range allowedChao {
-			if chao[2] == searchingForRarity {
-				return true
+		/*
+			if x == 100 { // character
+				return len(allowedCharacters) != 0 // if any characters allowed, return true
 			}
-		}
-		return false
+			searchingForRarity := strconv.Itoa(x)[0] // get byte
+			for _, chao := range allowedChao {
+				if chao[2] == searchingForRarity {
+					return true
+				}
+			}
+			return false
+		*/
+		return true // Don't check for duplicate Chao
 	}
-	chaoAllowed := func(chid string) bool {
+	//chaoAllowed := func(chid string) bool {
+	_ = func(chid string) bool {
 		// Checks if chid is in allowedChao
 		for _, chao := range allowedChao {
 			if chao == chid {
@@ -114,51 +115,63 @@ func GetRandomChaoRouletteItems(rarities []int64, allowedCharacters, allowedChao
 		}
 		return false
 	}
-	characterAllowed := func(cid string) bool {
+	//characterAllowed := func(cid string) bool {
+	_ = func(cid string) bool {
 		// Checks if cid is in allowedCharacters
-		for _, char := range allowedCharacters {
-			if char == cid {
-				return true
+		/*
+			for _, char := range allowedCharacters {
+				if char == cid {
+					return true
+				}
 			}
-		}
-		return false
+			return false
+		*/
+		return true // Don't check for duplicate characters
 	}
 
 	getUnusedChao := func(rarity int64) (string, error) {
 		// Implies that there is a Chao of the given rarity allowed! Check with allowedRarity.
 		chao, err := GetRandomChaoWheelChao(rarity, 1)
-		if err != nil {
-			return "", err
-		}
-		for !chaoAllowed(chao[0]) { // get allowed Chao
-			if time.Now().Unix()-failsafeTimeout > debugOriginalTime { // break out of long loop (debug)
-				return enums.ChaoIDStrHeroChao, nil
-			}
-			dprint("searching for allowed chao ", chao[0])
-			chao, err = GetRandomChaoWheelChao(rarity, 1)
+		// Don't check for unused Chao
+		return chao[0], err
+		/*
 			if err != nil {
 				return "", err
 			}
-		}
-		return chao[0], nil
+			for !chaoAllowed(chao[0]) { // get allowed Chao
+				if time.Now().Unix()-failsafeTimeout > debugOriginalTime { // break out of long loop (debug)
+					return enums.ChaoIDStrHeroChao, nil
+				}
+				dprint("searching for allowed chao ", chao[0])
+				chao, err = GetRandomChaoWheelChao(rarity, 1)
+				if err != nil {
+					return "", err
+				}
+			}
+			return chao[0], nil
+		*/
 	}
 	getUnusedCharacter := func() (string, error) {
 		// Implies that there are characters available! Check with len(allowedCharacters) != 0 or allowedRarity.
 		char, err := GetRandomChaoWheelCharacter(1)
-		if err != nil {
-			return "", err
-		}
-		for !characterAllowed(char[0]) {
-			if time.Now().Unix()-failsafeTimeout > debugOriginalTime { // break out of long loop (debug)
-				return enums.CTStrTails, nil
-			}
-			dprint("searching for allowed character ", char)
-			char, err = GetRandomChaoWheelCharacter(1)
+		// Don't check for unused characters
+		return char[0], err
+		/*
 			if err != nil {
 				return "", err
 			}
-		}
-		return char[0], nil
+			for !characterAllowed(char[0]) {
+				if time.Now().Unix()-failsafeTimeout > debugOriginalTime { // break out of long loop (debug)
+					return enums.CTStrTails, nil
+				}
+				dprint("searching for allowed character ", char)
+				char, err = GetRandomChaoWheelCharacter(1)
+				if err != nil {
+					return "", err
+				}
+			}
+			return char[0], nil
+		*/
 	}
 
 	newRarities := rarities
@@ -175,19 +188,6 @@ func GetRandomChaoRouletteItems(rarities []int64, allowedCharacters, allowedChao
 				items = append(items, char)
 			}
 		}
-		/*
-			if rarity == 3 { // Rarity 3 Chao
-				if !allowedRarity(3) { // if impossible
-					rarity = 2 // degrade
-				} else {
-					chao, err := getUnusedChao(3)
-					if err != nil {
-						return []string{}, []int64{}, err
-					}
-					items = append(items, chao)
-				}
-			}
-		*/
 		if rarity == 2 { // Rarity 2 Chao
 			if !allowedRarity(2) { // if impossible
 				rarity = 1 // degrade
