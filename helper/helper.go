76d32b5083dfe7ee3d1865a592d1977ae95127a5
fluofoxxo <35150778+fluofoxxo@users.noreply.github.com>
2019-11-16 17:38:43 -0500

    Corrected const names in helper, boilerplate Helper.DebugOut

--- helper/helper.go
+++ helper/helper.go
@@ -15,19 +15,19 @@ import (
 )
 
 const (
-	// TODO: use proper naming conventions
-	PREFIX_ERR             = "ERR"
-	PREFIX_OUT             = "OUT"
-	PREFIX_WARN            = "WARN"
-	PREFIX_UNCATCHABLE_ERR = "UNCATCHABLE ERR"
+	PrefixErr            = "ERR"
+	PrefixOut            = "OUT"
+	PrefixWarn           = "WARN"
+	PrefixUncatchableErr = "UNCATCHABLE ERR"
+	PrefixDebugOut       = "DEBUG (OUT)"
 
-	LOGOUT_BASE = "[%s] (%s) %s\n"
-	LOGERR_BASE = "[%s] (%s) %s: %s\n"
+	LogOutBase = "[%s] (%s) %s\n"
+	LogErrBase = "[%s] (%s) %s: %s\n"
 
-	INTERNAL_SRV_ERR = "Internal server error"
-	BAD_REQUEST      = "Bad request"
+	InternalServerError = "Internal server error"
+	BadRequest          = "Bad request"
 
-	DEFAULT_IV = "HotAndSunnyMiami"
+	DefaultIV = "HotAndSunnyMiami"
 )
 
 type Helper struct {
@@ -86,55 +86,58 @@ func (r *Helper) RespondRaw(out []byte, secureFlag, iv string) {
 	r.RespW.Write(toClient)
 }
 func (r *Helper) Respond(out []byte) {
-	r.RespondRaw(out, "1", DEFAULT_IV)
+	r.RespondRaw(out, "1", DefaultIV)
 }
 func (r *Helper) RespondInsecure(out []byte) {
 	r.RespondRaw(out, "0", "")
 }
 func (r *Helper) Out(msg string) {
-	log.Printf(LOGOUT_BASE, PREFIX_OUT, r.CallerName, msg)
+	log.Printf(LogOutBase, PrefixOut, r.CallerName, msg)
+}
+func (r *Helper) DebugOut(msg string) {
+	log.Printf(LogOutBase, PrefixDebugOut, r.CallerName, msg)
 }
 func (r *Helper) Warn(msg string) {
-	log.Printf(LOGOUT_BASE, PREFIX_WARN, r.CallerName, msg)
+	log.Printf(LogOutBase, PrefixWarn, r.CallerName, msg)
 }
 func (r *Helper) WarnErr(msg string, err error) {
-	log.Printf(LOGERR_BASE, PREFIX_WARN, r.CallerName, msg, err.Error())
+	log.Printf(LogErrBase, PrefixWarn, r.CallerName, msg, err.Error())
 }
 func (r *Helper) Uncatchable(msg string) {
-	log.Printf(LOGOUT_BASE, PREFIX_OUT, r.CallerName, msg)
+	log.Printf(LogOutBase, PrefixOut, r.CallerName, msg)
 }
 func (r *Helper) InternalErr(msg string, err error) {
-	log.Printf(LOGERR_BASE, PREFIX_ERR, r.CallerName, msg, err.Error())
+	log.Printf(LogErrBase, PrefixErr, r.CallerName, msg, err.Error())
 	r.RespW.WriteHeader(http.StatusBadRequest)
-	r.RespW.Write([]byte(BAD_REQUEST))
+	r.RespW.Write([]byte(BadRequest))
 }
 func (r *Helper) Err(msg string, err error) {
-	log.Printf(LOGERR_BASE, PREFIX_ERR, r.CallerName, msg, err.Error())
+	log.Printf(LogErrBase, PrefixErr, r.CallerName, msg, err.Error())
 	r.RespW.WriteHeader(http.StatusBadRequest)
-	r.RespW.Write([]byte(BAD_REQUEST))
+	r.RespW.Write([]byte(BadRequest))
 }
 func (r *Helper) ErrRespond(msg string, err error, response string) {
 	// TODO: remove if never used in stable builds
-	log.Printf(LOGERR_BASE, PREFIX_ERR, r.CallerName, msg, err.Error())
+	log.Printf(LogErrBase, PrefixErr, r.CallerName, msg, err.Error())
 	r.RespW.WriteHeader(http.StatusInternalServerError) // ideally include an option for this, but for now it's inconsequential
 	r.RespW.Write([]byte(response))
 }
 func (r *Helper) InternalFatal(msg string, err error) {
-	log.Fatalf(LOGERR_BASE, PREFIX_ERR, r.CallerName, msg, err.Error())
+	log.Fatalf(LogErrBase, PrefixErr, r.CallerName, msg, err.Error())
 	r.RespW.WriteHeader(http.StatusBadRequest)
-	r.RespW.Write([]byte(BAD_REQUEST))
+	r.RespW.Write([]byte(BadRequest))
 }
 func (r *Helper) Fatal(msg string, err error) {
-	log.Fatalf(LOGERR_BASE, PREFIX_ERR, r.CallerName, msg, err.Error())
+	log.Fatalf(LogErrBase, PrefixErr, r.CallerName, msg, err.Error())
 	r.RespW.WriteHeader(http.StatusBadRequest)
-	r.RespW.Write([]byte(BAD_REQUEST))
+	r.RespW.Write([]byte(BadRequest))
 }
 func (r *Helper) BaseInfo(em string, statusCode int64) responseobjs.BaseInfo {
 	return responseobjs.NewBaseInfo(em, statusCode)
 }
 func (r *Helper) InvalidRequest() {
 	r.RespW.WriteHeader(http.StatusBadRequest)
-	r.RespW.Write([]byte(BAD_REQUEST))
+	r.RespW.Write([]byte(BadRequest))
 }
 func (r *Helper) GetCallingPlayer() (netobj.Player, error) {
 	// Powerful function to get the player directly from the response
